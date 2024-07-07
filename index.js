88dfc5053eaff169c9b0a12cff17ad9b47ada9cd
Lethabo Mathabatha <lethabomathabatha.m@gmail.com>
2024-07-07 22:41:01 +0200

    add initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+const inputBtn = document.getElementById('input-btn'); 
+const tabBtn = document.getElementById('tab-btn');
+const deleteBtn = document.getElementById('delete-btn');
+const inputEl = document.getElementById('input-el');
+let inputValue = document.getElementById('input-el').value;
+
+inputBtn.addEventListener('click', () => {
+    alert('input btn clicked');
+})
